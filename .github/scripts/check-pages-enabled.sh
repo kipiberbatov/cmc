0bf33eaab620d2e898885670a2792704e426a078
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-06-19 11:47:11 +0000

    Change manual secret GH_PAT with GITHUB_TOKEN

--- .github/scripts/check-pages-enabled.sh
+++ .github/scripts/check-pages-enabled.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-if [[ -z "$GH_PAT" ]]; then
-  echo "GH_PAT secret is not set." >&2
-  echo "The static website will not be deployed to GitHub Pages." >&2
-  echo "enabled=false" >> "$GITHUB_OUTPUT"
-  exit 0
-fi
-
 echo "Checking for existence of environment: 'github-pages'"
 
 HTTP_STATUS=$(curl --write-out "%{http_code}" --silent --output /dev/null \
   --header "Accept: application/vnd.github.v3+json" \
-  --header "Authorization: Bearer $GH_PAT" \
+  --header "Authorization: Bearer $GH_TOKEN" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/environments/github-pages")
 
 if [[ "$HTTP_STATUS" -eq 200 ]]; then
