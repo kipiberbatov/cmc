613f89d7bf66bc7702a0cd5939381d52b00d4e81
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2025-06-20 08:20:25 +0000

    Remove environment variable GH_TOKEN

--- .github/scripts/check-pages-enabled.sh
+++ .github/scripts/check-pages-enabled.sh
@@ -6,7 +6,6 @@ echo "Checking for existence of environment: 'github-pages'"
 
 HTTP_STATUS=$(curl --write-out "%{http_code}" --silent --output /dev/null \
   --header "Accept: application/vnd.github.v3+json" \
-  --header "Authorization: Bearer $GH_TOKEN" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/environments/github-pages")
 
 if [[ "$HTTP_STATUS" -eq 200 ]]; then
