9eecedd96c904c711e84774b7e54bc5ffd1d2862
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2026-01-08 14:48:07 +0000

    Add diffusion main files for compilation by MinGW

--- mingw.sh
+++ mingw.sh
@@ -5,7 +5,7 @@
 set -x
 
 make OS=Windows_NT MODE=windows/release CC=x86_64-w64-mingw32-gcc obj_src
-make OS=Windows_NT MODE=windows/release CC=x86_64-w64-mingw32-gcc obj_main_{array,algebra,region,mesh}
+make OS=Windows_NT MODE=windows/release CC=x86_64-w64-mingw32-gcc obj_main_{array,algebra,region,mesh,diffusion}
 make OS=Windows_NT MODE=windows/release CC=x86_64-w64-mingw32-gcc obj_plugins_diffusion
 make OS=Windows_NT MODE=windows/release AR=x86_64-w64-mingw32-ar lib_src
 make OS=Windows_NT MODE=windows/release CC=x86_64-w64-mingw32-gcc lib_plugins_diffusion
