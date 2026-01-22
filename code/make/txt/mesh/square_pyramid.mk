dbcd5f2bfe641e3643aa6b1ac1dc2b88c4ca9f60
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2026-01-22 21:32:39 +0000

    Move square_pyramid to a separate directory

--- code/make/txt/mesh/square_pyramid.mk
+++ code/make/txt/mesh/square_pyramid.mk
@@ -1,15 +1,21 @@
 .PHONY: txt_mesh_square_pyramid txt_mesh_square_pyramid_clean
 
 _txt_mesh_square_pyramid := \
-  build/$(MODE)/txt/mesh/square_pyramid_forman.txt\
+  build/$(MODE)/txt/mesh/square_pyramid/forman.txt\
 
-build/$(MODE)/txt/mesh/square_pyramid_forman.txt:\
+build/$(MODE)/txt/mesh/square_pyramid: | build/$(MODE)/txt/mesh
+	mkdir -p $@
+
+build/$(MODE)/txt/mesh/square_pyramid/forman.txt:\
   build/$(MODE)/bin/forman$(.EXE)\
   data/mesh/square_pyramid.txt\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/square_pyramid
 	$(INTERPRETER) $< --raw $(word 2, $^) --standard --raw > $@
 
 txt_mesh_square_pyramid: $(_txt_mesh_square_pyramid)
 
 txt_mesh_square_pyramid_clean:
 	-$(RM) $(_txt_mesh_square_pyramid)
+
+txt_mesh_square_pyramid_distclean:
+	-$(RM) -r build/$(MODE)/txt/mesh/square_pyramid
