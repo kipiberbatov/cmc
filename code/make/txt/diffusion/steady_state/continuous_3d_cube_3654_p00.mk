fddcff91f92825ae30d6290f66c82580dc2bd77c
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2026-01-22 22:54:08 +0000

    Add mesh sandstone_3062_grains to a separate directory

--- code/make/txt/diffusion/steady_state/continuous_3d_cube_3654_p00.mk
+++ code/make/txt/diffusion/steady_state/continuous_3d_cube_3654_p00.mk
@@ -30,7 +30,7 @@ _txt_diffusion_steady_state_continuous_3d_cube_3654_p00 :=\
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/exact_sandstone_3062_grains_forman_potential.txt:\
   build/$(MODE)/bin/diffusion_steady_state_continuous_exact_potential$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
   build/$(MODE)/obj/plugins/diffusion_steady_state_continuous_3d_cube_3654_p00$(.OBJ)\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00\
     build/$(MODE)/lib/plugins/libdiffusion$(.DLL)
@@ -51,7 +51,7 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/exact_sands
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_input.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_primal_strong_from_continuous$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
   build/$(MODE)/obj/plugins/diffusion_steady_state_continuous_3d_cube_3654_p00$(.OBJ)\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00\
     build/$(MODE)/lib/plugins/libdiffusion$(.DLL)
@@ -63,9 +63,9 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_stro
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_potential.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_primal_strong_solve$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd_star.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd_star.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_input.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
 	$(INTERPRETER) $<\
@@ -77,8 +77,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_stro
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_flow_rate.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_flow_rate_from_potential$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_strong_cochain_sandstone_3062_grains_forman_potential.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
@@ -121,8 +121,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_stro
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_input.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_primal_weak_from_continuous$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt\
   build/$(MODE)/obj/plugins/diffusion_steady_state_continuous_3d_cube_3654_p00$(.OBJ)\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00\
     build/$(MODE)/lib/plugins/libdiffusion$(.DLL)
@@ -135,8 +135,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_potential.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_primal_weak_solve$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_input.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
 	$(INTERPRETER) $<\
@@ -147,8 +147,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_flow_rate.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_flow_rate_from_potential$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak_cochain_sandstone_3062_grains_forman_potential.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
@@ -191,8 +191,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/primal_weak
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_input.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_mixed_weak_from_continuous$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt\
   build/$(MODE)/obj/plugins/diffusion_steady_state_continuous_3d_cube_3654_p00$(.OBJ)\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00\
     build/$(MODE)/lib/plugins/libdiffusion$(.DLL)
@@ -205,8 +205,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_solution.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_mixed_weak_solve$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_input.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
 	$(INTERPRETER) $<\
@@ -217,8 +217,8 @@ build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_
 
 build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_potential.txt:\
   build/$(MODE)/bin/diffusion_steady_state_discrete_mixed_weak_potential$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_input.txt\
   build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00/mixed_weak_cochain_sandstone_3062_grains_forman_solution.txt\
   | build/$(MODE)/txt/diffusion/steady_state/continuous_3d_cube_3654_p00
--- code/make/txt/mesh/sandstone_3062_grains.mk
+++ code/make/txt/mesh/sandstone_3062_grains.mk
@@ -1,15 +1,15 @@
 .PHONY: txt_mesh_sandstone_3062_grains txt_mesh_sandstone_3062_grains_clean
 
 _txt_mesh_sandstone_3062_grains :=\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_kappa.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt\
-  # build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt\
-  # build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd_star.txt\
-  # build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge_coeff.txt\
-  # build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/data.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/kappa.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt\
+  # build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt\
+  # build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd_star.txt\
+  # build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge_coeff.txt\
+  # build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge.txt\
 
 assets/mesh/sandstone_3062_grains.tess: | assets/mesh
 	curl https://zenodo.org/records/18248963/files/sandstone_3062_grains.tess > $@
@@ -26,69 +26,75 @@ assets/mesh/sandstone_3062_grains_vpore.csv: | assets/mesh
 assets/mesh/sandstone_3062_grains_fpore.csv: | assets/mesh
 	curl https://zenodo.org/records/18248963/files/sandstone_3062_grains_fpore.csv > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains: | build/$(MODE)/txt/mesh/sandstone_3062_grains
+	mkdir -p $@
+
+build/$(MODE)/txt/mesh/sandstone_3062_grains/data.txt:\
   build/$(MODE)/bin/mesh_and_boundary_file_scan$(.EXE)\
   assets/mesh/sandstone_3062_grains.tess\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< --mesh-format=tess $(word 2, $^) > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_kappa.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/kappa.txt:\
   build/$(MODE)/bin/mesh_generate_3d_kappa$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/data.txt\
   assets/mesh/sandstone_3062_grains.stface\
   assets/mesh/sandstone_3062_grains.stcell\
   assets/mesh/sandstone_3062_grains_vpore.csv\
   assets/mesh/sandstone_3062_grains_fpore.csv\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< --raw $(word 2, $^) $(word 3, $^) $(word 4, $^) $(word 5, $^) $(word 6, $^) 0.0651 74 6526 > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt:\
   build/$(MODE)/bin/forman_boundary$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/data.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< --raw $(word 2, $^) --standard --raw > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd.txt:\
   build/$(MODE)/bin/mesh_coboundary$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< --raw < $(word 2, $^) > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt:\
   build/$(MODE)/bin/mesh_qc_vol$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< < $(word 2, $^) > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt:\
   build/$(MODE)/bin/mesh_qc_inner_direct$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_vol.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_vol.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $^ > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_cbd_star.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_cbd_star.txt:\
   build/$(MODE)/bin/mesh_qc_coboundary_star$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $^ > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge_coeff.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge_coeff.txt:\
   build/$(MODE)/bin/mesh_qc_hodge_coeff$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $< < $(word 2, $^) > $@
 
-build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge.txt:\
+build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge.txt:\
   build/$(MODE)/bin/mesh_qc_hodge$(.EXE)\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_inner.txt\
-  build/$(MODE)/txt/mesh/sandstone_3062_grains_forman_hodge_coeff.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_inner.txt\
+  build/$(MODE)/txt/mesh/sandstone_3062_grains/forman_hodge_coeff.txt\
+  | build/$(MODE)/txt/mesh/sandstone_3062_grains
 	$(INTERPRETER) $^ > $@
 
 txt_mesh_sandstone_3062_grains: $(_txt_mesh_sandstone_3062_grains)
 
 txt_mesh_sandstone_3062_grains_clean:
 	-$(RM) $(_txt_mesh_sandstone_3062_grains)
+
+txt_mesh_sandstone_3062_grains_distclean:
+	-$(RM) -r build/$(MODE)/txt/mesh/sandstone_3062_grains
