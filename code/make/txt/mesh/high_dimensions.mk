75db7052d8f6886c0de609a7c3a77417d85d64ad
Kiprian Berbatov <kiprian.berbatov@manchester.ac.uk>
2026-01-22 22:45:41 +0000

    Add meshes brick_high_dimensions to a separate directory

--- code/make/txt/mesh/high_dimensions.mk
+++ code/make/txt/mesh/high_dimensions.mk
@@ -2,45 +2,51 @@
 
 # dimensions 8, 9, 10
 _txt_mesh_high_dimensions :=\
-  build/$(MODE)/txt/mesh/brick_8d_1.txt\
-  build/$(MODE)/txt/mesh/brick_8d_2.txt\
-  build/$(MODE)/txt/mesh/brick_9d_1.txt\
-  build/$(MODE)/txt/mesh/brick_9d_2.txt\
-  build/$(MODE)/txt/mesh/brick_10d_1.txt\
-  build/$(MODE)/txt/mesh/brick_8d_1_forman.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/8d_1.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/8d_2.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/9d_1.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/9d_2.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/10d_1.txt\
+  build/$(MODE)/txt/mesh/brick_high_dimensions/8d_1_forman.txt\
 
-build/$(MODE)/txt/mesh/brick_8d_1.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions: | build/$(MODE)/txt/mesh
+	mkdir -p $@
+
+build/$(MODE)/txt/mesh/brick_high_dimensions/8d_1.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< 8 1 > $@
 
-build/$(MODE)/txt/mesh/brick_8d_2.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions/8d_2.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< 8 2 > $@
 
-build/$(MODE)/txt/mesh/brick_9d_1.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions/9d_1.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< 9 1 > $@
 
-build/$(MODE)/txt/mesh/brick_9d_2.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions/9d_2.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< 9 2 > $@
 
-build/$(MODE)/txt/mesh/brick_10d_1.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions/10d_1.txt:\
   build/$(MODE)/bin/mesh_brick_regular$(.EXE)\
-  | build/$(MODE)/txt/mesh
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< 10 1 > $@
 
-build/$(MODE)/txt/mesh/brick_8d_1_forman.txt:\
+build/$(MODE)/txt/mesh/brick_high_dimensions/8d_1_forman.txt:\
   build/$(MODE)/bin/forman_boundary$(.EXE)\
-  build/$(MODE)/txt/mesh/brick_8d_1.txt\
-  | build/$(MODE)/txt/mesh
+  build/$(MODE)/txt/mesh/brick_high_dimensions/8d_1.txt\
+  | build/$(MODE)/txt/mesh/brick_high_dimensions
 	$(INTERPRETER) $< --raw $(word 2, $^) --standard --raw > $@
 
 txt_mesh_high_dimensions: $(_txt_mesh_high_dimensions)
 
 txt_mesh_high_dimensions_clean:
 	-$(RM) $(_txt_mesh_high_dimensions)
+
+txt_mesh_high_dimensions_distclean:
+	-$(RM) -r build/$(MODE)/txt/mesh/brick_high_dimensions
